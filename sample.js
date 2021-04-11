b8cc1988df8b6dd6f2437ac9afd46a4f57847047
shaRunO <sharon.adega@gmail.com>
2021-04-11 22:21:10 +0100

    fist push to github for responsive website

---
+++ sample.js
@@ -0,0 +1,7 @@
+const menu = document.queryselector('#mobile-menu');
+const menuLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", function() {
+    menu.classlist.toggle("is-active");
+    menulinks.classlist.toggle("active");
+})
